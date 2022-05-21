bcc5fe1e18eaee683bbe6a13db911658375d2040
Ellen Poe <ellen@ellenhp.me>
2022-05-20 22:22:44 -0700

    Loading postgis with data now!

--- postgis_deploy/deploy.sh
+++ postgis_deploy/deploy.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 
 files=(*.pgsql.tgz)
 
 tar xvf ${files[0]}
 
-psql -h postgis -U postgis_user -d postgis_db -a -f ./load.sql
+export PGPASSWORD=god
+
+psql -h postgis -U postgis -d postgis -a -f ./load.sql
--- postgis_deploy/load.sql
+++ postgis_deploy/load.sql
@@ -28,12 +28,113 @@ DROP TABLE IF EXISTS "new_users";
 DROP TABLE IF EXISTS "new_way_nodes";
 DROP TABLE IF EXISTS "new_ways";
 
+CREATE EXTENSION hstore;
+
+CREATE TABLE IF NOT EXISTS schema_info (
+    version integer NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL,
+    name text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS nodes (
+    id bigint NOT NULL,
+    version int NOT NULL,
+    user_id int NOT NULL,
+    tstamp timestamp without time zone NOT NULL,
+    changeset_id bigint NOT NULL,
+    tags hstore
+);
+SELECT AddGeometryColumn('nodes', 'geom', 4326, 'POINT', 2);
+
+CREATE TABLE IF NOT EXISTS node_tags (
+    node_id bigint NOT NULL,
+    k text NOT NULL,
+    v text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ways (
+    id bigint NOT NULL,
+    version int NOT NULL,
+    user_id int NOT NULL,
+    tstamp timestamp without time zone NOT NULL,
+    changeset_id bigint NOT NULL,
+    tags hstore,
+    nodes bigint[]
+);
+SELECT AddGeometryColumn('ways', 'bbox', 4326, 'GEOMETRY', 2);
+SELECT AddGeometryColumn('ways', 'linestring', 4326, 'GEOMETRY', 2);
+
+CREATE TABLE IF NOT EXISTS way_nodes (
+    way_id bigint NOT NULL,
+    node_id bigint NOT NULL,
+    sequence_id int NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS way_tags (
+    way_id bigint NOT NULL,
+    k text NOT NULL,
+    v text
+);
+
+CREATE TABLE IF NOT EXISTS relations (
+    id bigint NOT NULL,
+    version int NOT NULL,
+    user_id int NOT NULL,
+    tstamp timestamp without time zone NOT NULL,
+    changeset_id bigint NOT NULL,
+    tags hstore
+);
+
+CREATE TABLE IF NOT EXISTS relation_members (
+    relation_id bigint NOT NULL,
+    member_id bigint NOT NULL,
+    member_type character(1) NOT NULL,
+    member_role text NOT NULL,
+    sequence_id int NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS relation_tags (
+    relation_id bigint NOT NULL,
+    k text NOT NULL,
+    v text NOT NULL
+);
+
+INSERT INTO schema_info (version)
+SELECT 5
+WHERE NOT EXISTS (SELECT * FROM schema_info);
+
+ALTER TABLE ONLY users ADD CONSTRAINT pk_users PRIMARY KEY (id);
+
+ALTER TABLE ONLY nodes ADD CONSTRAINT pk_nodes PRIMARY KEY (id);
+
+ALTER TABLE ONLY ways ADD CONSTRAINT pk_ways PRIMARY KEY (id);
+
+ALTER TABLE ONLY way_nodes ADD CONSTRAINT pk_way_nodes PRIMARY KEY (way_id, sequence_id);
+
+ALTER TABLE ONLY relations ADD CONSTRAINT pk_relations PRIMARY KEY (id);
+
+ALTER TABLE ONLY relation_members ADD CONSTRAINT pk_relation_members PRIMARY KEY (relation_id, sequence_id);
+
+CREATE INDEX idx_nodes_geom ON nodes USING gist (geom);
+CREATE INDEX idx_way_nodes_node_id ON way_nodes USING btree (node_id);
+CREATE INDEX idx_ways_bbox ON ways USING gist (bbox);
+CREATE INDEX idx_ways_linestring ON ways USING gist (linestring);
+CREATE INDEX idx_node_tags_node_id ON node_tags USING btree (node_id);
+CREATE INDEX idx_way_tags_way_id ON way_tags USING btree (way_id);
+CREATE INDEX idx_relation_tags_relation_id ON relation_tags USING btree (relation_id);
+
 CREATE TABLE "new_nodes" (LIKE "nodes");
 CREATE TABLE "new_relation_members" (LIKE "relation_members");
 CREATE TABLE "new_relations" (LIKE "relations");
 CREATE TABLE "new_users" (LIKE "users");
 CREATE TABLE "new_way_nodes" (LIKE "way_nodes");
 CREATE TABLE "new_ways" (LIKE "ways");
+CREATE TABLE "new_node_tags" (LIKE "node_tags");
+CREATE TABLE "new_way_tags" (LIKE "way_tags");
+CREATE TABLE "new_relation_tags" (LIKE "relation_tags");
 
 \copy new_nodes FROM 'nodes.txt'
 \copy new_relation_members FROM 'relation_members.txt'
