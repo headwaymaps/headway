30fa8cccb1870e759edd7c56d752d8408f123236
Ellen <ellen@ellenhp.me>
2022-05-24 18:18:15 +0000

    Small tweaks to max bounds behavior

--- web/frontend/src/components/BaseMap.vue
+++ web/frontend/src/components/BaseMap.vue
@@ -21,9 +21,10 @@ async function loadMap() {
     parseFloat(bbox_strings[2]),
     parseFloat(bbox_strings[3]),
   ];
-  const extents = [bounds[2] - bounds[0], bounds[3] - bounds[1]]
-  const maxExtent = Math.max(...extents) / 2
   const center = [(bounds[2] + bounds[0]) / 2, (bounds[3] + bounds[1]) / 2]
+  const scaleFactor = 1.0 / Math.cos(3.14159 / 180 * center[1])
+  const extents = [bounds[2] - bounds[0], bounds[3] - bounds[1]]
+  const maxExtent = Math.max(...extents) * scaleFactor
   const maxBounds = [center[0] - maxExtent, center[1] - maxExtent, center[0] + maxExtent, center[1] + maxExtent]
   map = new maplibregl.Map({
     container: 'map', // container id
