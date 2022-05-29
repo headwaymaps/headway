e5ceac1cf0fdbedb0abd7934d9ce90813b9e5160
3np <3np@example.com>
2022-05-29 14:02:54 +0900

    mbtiles: streaming sources fetch+extract

--- mbtiles/bootstrap/download_to_volume.sh
+++ mbtiles/bootstrap/download_to_volume.sh
@@ -4,6 +4,4 @@ cd /data
 
 mkdir -p sources
 cd sources
-rm -f *.tar
-ls *.zip || wget https://f000.backblazeb2.com/file/headway/sources.tar && tar xvf sources.tar
-rm -f *.tar
+ls *.zip || (curl https://f000.backblazeb2.com/file/headway/sources.tar | tar xv)
