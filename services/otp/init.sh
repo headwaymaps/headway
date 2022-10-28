948f016fb4685cedbd57b9b820b0f80850f40767
Michael Kirk <michael.code@endoftheworl.de>
2022-10-28 13:51:34 -0700

    disambiguate env vars for easier grepping

--- services/otp/init.sh
+++ services/otp/init.sh
@@ -2,16 +2,16 @@
 
 set -xe
 
-if [ -f "${ARTIFACT_DEST_PATH}" ]; then
-    echo "Nothing to do, already have ${ARTIFACT_DEST_PATH}"
+if [ -f "${OTP_ARTIFACT_DEST_PATH}" ]; then
+    echo "Nothing to do, already have ${OTP_ARTIFACT_DEST_PATH}"
     exit 0
 fi
 
-if [ -f "${ARTIFACT_SOURCE_PATH}" ]; then
+if [ -f "${OTP_ARTIFACT_SOURCE_PATH}" ]; then
     echo "Copying artifact."
-    cp "${ARTIFACT_SOURCE_PATH}" "${ARTIFACT_DEST_PATH}"
+    cp "${OTP_ARTIFACT_SOURCE_PATH}" "${OTP_ARTIFACT_DEST_PATH}"
     exit 0
 fi
 
 echo "Downloading artifact"
-wget -O "${ARTIFACT_DEST_PATH}" "${ARTIFACT_URL}"
+wget -O "${OTP_ARTIFACT_DEST_PATH}" "${OTP_ARTIFACT_URL}"
--- services/valhalla/init.sh
+++ services/valhalla/init.sh
@@ -3,25 +3,25 @@
 set -xe
 
 
-if [ -f "${ARTIFACT_LOCK}" ]; then
+if [ -f "${VALHALLA_ARTIFACT_LOCK}" ]; then
     echo "Nothing to do, already have artifact."
     exit 0
 fi
 
 mkdir -p /data/valhalla/
 
-if [ -f "${ARTIFACT_SOURCE_PATH}" ]; then
+if [ -f "${VALHALLA_ARTIFACT_SOURCE_PATH}" ]; then
     echo "Copying artifact."
-    cd /data && cp ${ARTIFACT_SOURCE_PATH} /data/valhalla/tiles.tar
+    cd /data && cp ${VALHALLA_ARTIFACT_SOURCE_PATH} /data/valhalla/tiles.tar
 else
     echo "Downloading artifact."
-    cd /data && wget -O /data/valhalla/tiles.tar "${ARTIFACT_URL}"
+    cd /data && wget -O /data/valhalla/tiles.tar "${VALHALLA_ARTIFACT_URL}"
 fi
 
 valhalla_build_config --mjolnir-timezone /data/timezones.sqlite --mjolnir-admin /data/admins.sqlite > /data/valhalla.json
 
 chown -R valhalla /data
 
-touch ${ARTIFACT_LOCK}
+touch ${VALHALLA_ARTIFACT_LOCK}
 
 ls -lR /data
