27ebf66789aa036279059f8c8919a0a1aba49de7
Michael Kirk <michael.code@endoftheworl.de>
2022-11-15 00:11:16 +0000

    [deploy] more robust download scripts
    - fail script if download (or any pipe) fails
    - wait for complete download rather than downloading in place
    - retry more times and continue partial download

--- services/pelias/init_elastic.sh
+++ services/pelias/init_elastic.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 set -xe
-
-mkdir -p /usr/share/elasticsearch/data
+set -o pipefail
 
 if [ ! -z "$(find /usr/share/elasticsearch/data -type f)" ]; then
     echo "Nothing to do, already have elasticsearch data"
 elif [ -f "${ELASTICSEARCH_ARTIFACT_SOURCE_PATH}" ]; then
     echo "Extracting artifact."
+    mkdir -p /usr/share/elasticsearch/data
     tar -xJf "${ELASTICSEARCH_ARTIFACT_SOURCE_PATH}" -C /usr/share/elasticsearch/data
 else
     echo "Downloading and extracting artifact."
-    wget -O- "${ELASTICSEARCH_ARTIFACT_URL}" | tar -xJ -C /usr/share/elasticsearch/data
+    rm -fr /tmp/elasticsearch.download
+    mkdir -p /tmp/elasticsearch.download
+    wget --tries=100 -O- "${ELASTICSEARCH_ARTIFACT_URL}" | tar -xJ -C /tmp/elasticsearch.download
+    mv /tmp/elasticsearch.download/nodes /usr/share/elasticsearch/data/nodes
+    rmdir /tmp/elasticsearch.download
 fi
--- services/valhalla/init.sh
+++ services/valhalla/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -xe
+set -o pipefail
 
-
-if [ -f "${VALHALLA_ARTIFACT_LOCK}" ]; then
+if [ -f /data/valhalla/tiles.tar ]; then
     echo "Nothing to do, already have artifact."
     exit 0
 fi
@@ -12,16 +12,15 @@ mkdir -p /data/valhalla/
 
 if [ -f "${VALHALLA_ARTIFACT_SOURCE_PATH}" ]; then
     echo "Copying artifact."
-    cd /data && xz --decompress --stdout ${VALHALLA_ARTIFACT_SOURCE_PATH} > /data/valhalla/tiles.tar
+    xz --decompress --stdout ${VALHALLA_ARTIFACT_SOURCE_PATH} > /data/valhalla/tiles.tar
 else
     echo "Downloading artifact."
-    cd /data && wget -O- "${VALHALLA_ARTIFACT_URL}" | xz --decompress --stdout > /data/valhalla/tiles.tar
+    wget --tries=100 -O- "${VALHALLA_ARTIFACT_URL}" | xz --decompress --stdout > /data/valhalla/tiles.tar.download
+    mv /data/valhalla/tiles.tar.download /data/valhalla/tiles.tar
 fi
 
 valhalla_build_config --mjolnir-timezone /data/timezones.sqlite --mjolnir-admin /data/admins.sqlite > /data/valhalla.json
 
 chown -R valhalla /data
-
-touch ${VALHALLA_ARTIFACT_LOCK}
 
 ls -lR /data
