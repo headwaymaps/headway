20212381667ccd3b82975818b16345031a0c20f2
Michael Kirk <michael.code@endoftheworl.de>
2022-12-09 11:25:45 -0800

    [www] some incidental cleanup

--- web/frontend/src/components/SearchBox.vue
+++ web/frontend/src/components/SearchBox.vue
@@ -11,17 +11,16 @@
       :debounce="0"
       :dense="true"
       v-on:clear="() => selectPoi(undefined)"
-      v-on:blur="deferHide(castToMenu($refs.autoCompleteMenu))"
+      v-on:blur="deferHide(autoCompleteMenu())"
       v-on:beforeinput="
-        (event) =>
-          updateAutocompleteEventBeforeInput(
-            event,
-            castToMenu($refs.autoCompleteMenu)
-          )
+  (event: Event) =>
+    updateAutocompleteEventBeforeInput(
+      event,
+      autoCompleteMenu()
+      )
       "
       v-on:update:model-value="
-        () =>
-          updateAutocompleteEventRawString(castToMenu($refs.autoCompleteMenu))
+        () => updateAutocompleteEventRawString(autoCompleteMenu())
       "
     >
     </q-input>
@@ -31,25 +30,27 @@
       :no-focus="true"
       :no-refocus="true"
       v-on:before-hide="removeHoverMarkers"
-      :target="castToTarget($refs.autoCompleteInput)"
+      :target="($refs.autoCompleteInput as Element)"
     >
-      <q-item
-        :key="item?.key"
-        v-for="item in autocompleteOptions"
-        clickable
-        v-on:click="() => selectPoi(item)"
-        v-on:mouseenter="() => hoverPoi(item)"
-        v-on:mouseleave="() => hoverPoi(undefined)"
-      >
-        <q-item-section>
-          <q-item-label>{{
-            item?.name ? item.name : item?.address
-          }}</q-item-label>
-          <q-item-label v-if="item?.name" caption>{{
-            item.address
-          }}</q-item-label>
-        </q-item-section>
-      </q-item>
+      <q-list>
+        <q-item
+          :key="item?.key"
+          v-for="item in autocompleteOptions"
+          clickable
+          v-on:click="selectPoi(item)"
+          v-on:mouseenter="hoverPoi(item)"
+          v-on:mouseleave="hoverPoi(undefined)"
+        >
+          <q-item-section>
+            <q-item-label>{{
+              item?.name ? item.name : item?.address
+            }}</q-item-label>
+            <q-item-label v-if="item?.name" caption>{{
+              item.address
+            }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
     </q-menu>
   </div>
 </template>
@@ -71,7 +72,11 @@ export default defineComponent({
     forceText: String,
     hint: String,
   },
-  methods: {},
+  methods: {
+    autoCompleteMenu(): QMenu {
+      return this.$refs.autoCompleteMenu as QMenu;
+    },
+  },
   watch: {
     forceText: {
       immediate: true,
@@ -160,14 +165,6 @@ export default defineComponent({
       inputText,
       autocompleteOptions,
       poiHovered,
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      castToTarget(target: any) {
-        return target as Element;
-      },
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      castToMenu(menu: any) {
-        return menu as QMenu;
-      },
       deferHide(menu: QMenu) {
         setTimeout(() => {
           menu.hide();
