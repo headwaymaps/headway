9008f70afaf42a6fcd7284e1137ba4cff1a55281
Michael Kirk <michael.code@endoftheworl.de>
2022-12-15 11:38:21 -0600

    [www] click to zoom to step for valhalla routes. (#204)

--- web/frontend/src/components/SingleModeSteps.vue
+++ web/frontend/src/components/SingleModeSteps.vue
@@ -1,31 +1,44 @@
 <template>
   <q-list>
-    <div
-      v-for="item in trip.valhallaRoute.legs[0].maneuvers"
-      v-bind:key="JSON.stringify(item)"
+    <q-item
+      class="maneuver"
+      active-class="bg-blue-1"
+      v-for="maneuver in trip.valhallaRoute.legs[0].maneuvers"
+      clickable
+      v-on:click="clickedManeuver(maneuver)"
+      v-bind:key="JSON.stringify(maneuver)"
     >
-      <q-item class="q-my-sm" active-class="bg-blue-1">
-        <q-item-section avatar>
-          <q-icon :name="valhallaTypeToIcon(item.type)" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>
-            {{ item.instruction }}
-          </q-item-label>
-          <q-item-label caption>
-            {{ item.verbal_post_transition_instruction }}
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-separator spaced />
-    </div>
+      <q-item-section avatar>
+        <q-icon :name="valhallaTypeToIcon(maneuver.type)" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>
+          {{ maneuver.instruction }}
+        </q-item-label>
+        <q-item-label caption>
+          {{ maneuver.verbal_post_transition_instruction }}
+        </q-item-label>
+      </q-item-section>
+    </q-item>
   </q-list>
 </template>
+
+<style lang="scss">
+.maneuver {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border-bottom: solid 1px #ddd;
+}
+</style>
 
 <script lang="ts">
 import Route from 'src/models/Route';
 import { defineComponent, PropType } from 'vue';
-import { valhallaTypeToIcon } from 'src/services/ValhallaClient';
+import {
+  ValhallaRouteLegManeuver,
+  valhallaTypeToIcon,
+} from 'src/services/ValhallaClient';
+import { getBaseMap } from './BaseMap.vue';
 
 export default defineComponent({
   name: 'SingleModeSteps',
@@ -35,8 +48,20 @@ export default defineComponent({
       required: true,
     },
   },
+  data(): {
+    geometry: GeoJSON.LineString;
+  } {
+    return {
+      geometry: this.trip.legs[0].geometry(),
+    };
+  },
   methods: {
     valhallaTypeToIcon,
+    clickedManeuver: function (maneuver: ValhallaRouteLegManeuver) {
+      const location = this.geometry.coordinates[maneuver.begin_shape_index];
+      let coord: [number, number] = [location[0], location[1]];
+      getBaseMap()?.flyTo(coord, 16);
+    },
   },
 });
 </script>
