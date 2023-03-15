614cdafdacb529892b4ade0a3a1c6ff00a779a30
Michael Kirk <michael.code@endoftheworl.de>
2023-03-15 13:56:55 -0700

    [www] Fix small discrepancy in width of search field between pages

--- services/frontend/www-app/src/pages/BaseMapPage.vue
+++ services/frontend/www-app/src/pages/BaseMapPage.vue
@@ -25,7 +25,7 @@
       z-index: 1;
       position: absolute;
       left: 0;
-      width: calc(33% - 32px);
+      width: calc(max(33%, 370px) - 32px);
       margin: 16px;
       border: none;
       padding: 0;
