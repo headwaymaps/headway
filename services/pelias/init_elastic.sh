3900f47c780fc4d3f05f52e6c4787f8da9d27c83
Michael Kirk <michael.code@endoftheworl.de>
2023-07-27 03:21:30 +0200

    adapt to es8

--- services/pelias/init_elastic.sh
+++ services/pelias/init_elastic.sh
@@ -3,19 +3,31 @@
 set -xe
 set -o pipefail
 
+function extract_elastic() {
+    # hardcoded in elasticsearch Dockerfile
+    local elasticsearch_group=1000
+
+    local extract_dir=/tmp/elasticsearch.extract
+
+    rm -fr /tmp/elasticsearch.extract
+    mkdir "$extract_dir"
+    tar --zstd -x -f - -C "$extract_dir"
+
+    chgrp -R "$elasticsearch_group" "$extract_dir"
+    chmod -R 'g+rwX' "$extract_dir"
+
+    rm -fr /usr/share/elasticsearch/data/*
+    mv "${extract_dir}"/* /usr/share/elasticsearch/data
+}
+
 if [ ! -z "$(find /usr/share/elasticsearch/data -type f)" ]; then
     echo "Nothing to do, already have elasticsearch data"
 elif [ -f "${ELASTICSEARCH_ARTIFACT_SOURCE_PATH}" ]; then
-    echo "Extracting artifact."
-    mkdir -p /usr/share/elasticsearch/data
-    tar --zstd -xf "${ELASTICSEARCH_ARTIFACT_SOURCE_PATH}" -C /usr/share/elasticsearch/data
+    echo "Extracting existing artifact."
+    cat "$ELASTICSEARCH_ARTIFACT_SOURCE_PATH" | extract_elastic
 elif [ ! -z "${ELASTICSEARCH_ARTIFACT_URL}" ]; then
     echo "Downloading and extracting artifact."
-    rm -fr /tmp/elasticsearch.download
-    mkdir -p /tmp/elasticsearch.download
-    wget --tries=100 -O- "${ELASTICSEARCH_ARTIFACT_URL}" | tar --zstd -x -C /tmp/elasticsearch.download
-    mv /tmp/elasticsearch.download/nodes /usr/share/elasticsearch/data/nodes
-    rmdir /tmp/elasticsearch.download
+    wget --tries=100 -O- "$ELASTICSEARCH_ARTIFACT_URL" | extract_elastic
 else
     echo "No elasticsearch artifact available."
     exit 1
