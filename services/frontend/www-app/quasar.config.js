4b6b63ac6c03dbec6255bf5e763f18ad358f1314
Michael Kirk <michael.code@endoftheworl.de>
2024-02-22 19:06:31 -0800

    DO NOT MERGE: dev setup

--- services/frontend/www-app/quasar.config.js
+++ services/frontend/www-app/quasar.config.js
@@ -55,7 +55,7 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16',
       },
 
-      vueRouterMode: 'history', // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
