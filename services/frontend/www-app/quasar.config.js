79e358fa17ee6474d1547c038c020825d4808184
Michael Kirk <michael.code@endoftheworl.de>
2024-03-01 10:51:50 -0800

    undo some dev changes

--- services/frontend/www-app/quasar.config.js
+++ services/frontend/www-app/quasar.config.js
@@ -55,9 +55,10 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16',
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // Use this for dev where we don't have mod_rewrite, otherwise refreshing page 404's
       // vueRouterMode: 'hash',
+
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -106,16 +107,16 @@ module.exports = configure(function (/* ctx */) {
           // rewrite: (path) => path.replace(/^\/tileserver/, ''),
         },
         '/pelias': {
-          // target: HEADWAY_HOST,
-          // changeOrigin: true,
-          target: 'http://0.0.0.0:4000',
-          rewrite: (path) => path.replace(/^\/pelias/, ''),
+          target: HEADWAY_HOST,
+          changeOrigin: true,
+          // target: 'http://0.0.0.0:4000',
+          // rewrite: (path) => path.replace(/^\/pelias/, ''),
         },
         '/travelmux': {
-          // target: HEADWAY_HOST,
-          // changeOrigin: true,
-          target: 'http://0.0.0.0:8000',
-          rewrite: (path) => path.replace(/^\/travelmux/, ''),
+          target: HEADWAY_HOST,
+          changeOrigin: true,
+          // target: 'http://0.0.0.0:8000',
+          // rewrite: (path) => path.replace(/^\/travelmux/, ''),
         },
       },
     },
