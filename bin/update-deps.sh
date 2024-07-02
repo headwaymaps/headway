6923c1133b91d939d0e4fbae30a73786e3d3a3db
Michael Kirk <michael.code@endoftheworl.de>
2024-07-01 17:42:18 -0700

    update deps

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -2,8 +2,9 @@ set -ex
 
 (cd services/frontend/www-app && yarn upgrade) \
     && (cd services/pelias/generate_config && yarn upgrade) \
-    && (cd services/tileserver/assets && npm update) \
     && (cd services/travelmux && cargo update) \
     && (cd services/gtfs/gtfout && cargo update) \
     && (cd builds/planet/assemble-planet-pbf && cargo update)
 
+# fails on macos
+# cd services/tileserver/assets && npm update
