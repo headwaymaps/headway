e714b90639ec2f073b7be8fa79ce55de069ae0fa
Michael Kirk <michael.code@endoftheworl.de>
2025-01-06 14:24:31 -0800

    fixup! Release from OSM mainline, rather than daylight

--- builds/planet/prebuild.sh
+++ builds/planet/prebuild.sh
@@ -23,8 +23,8 @@ fi
 
 # Parse YYMMDD into YYYY/MM/DD
 YEAR="20${PLANET_DATE:0:2}"
-MONTH="${PLANET_DATE:2:2}"
-DAY="${PLANET_DATE:4:2}"
+_MONTH="${PLANET_DATE:2:2}"
+_DAY="${PLANET_DATE:4:2}"
 
 # Construct the S3 URLs
 BASE_URL="https://osm-planet-us-west-2.s3.amazonaws.com/planet/pbf/${YEAR}"
@@ -32,43 +32,37 @@ FILE_NAME="planet-${PLANET_DATE}.osm.pbf"
 MD5_NAME="planet-${PLANET_DATE}.osm.pbf.md5"
 PBF_URL="${BASE_URL}/${FILE_NAME}"
 MD5_URL="${BASE_URL}/${MD5_NAME}"
-DESINATION_PATH="${REPO_ROOT}/${FILE_NAME}"
+DESTINATION_PATH="${REPO_ROOT}/maps-earth-planet-v1.${PLANET_DATE}.osm.pbf"
 
-if [ -f $DESTINATION_PATH ]; then
-    echo "${FILE_NAME} already exists."
-    exit 0
+if [ -f "$DESTINATION_PATH" ]; then
+  echo "${FILE_NAME} already exists."
+  exit 0
 fi
 
 # Download the .pbf file
 echo "Downloading ${FILE_NAME}..."
-aria2c --file-allocation=none --continue=true --max-connection-per-server=16 --split=16 --retry-wait=5 --max-tries=5 "$PBF_URL"
-
-if [ $? -ne 0 ]; then
+if ! aria2c --file-allocation=none --continue=true --max-connection-per-server=16 --split=16 --retry-wait=5 --max-tries=5 "$PBF_URL"
+then
   echo "Failed to download ${FILE_NAME}."
   exit 1
 fi
 
 # Download the .md5 file
 echo "Downloading ${MD5_NAME}..."
-aria2c --file-allocation=none --continue=true --max-connection-per-server=16 --split=16 --retry-wait=5 --max-tries=5 "$MD5_URL"
 
-if [ $? -ne 0 ]; then
+if ! aria2c --file-allocation=none --continue=true --max-connection-per-server=16 --split=16 --retry-wait=5 --max-tries=5 "$MD5_URL"
+then
   echo "Failed to download ${MD5_NAME}."
   exit 1
 fi
 
 # Verify the MD5 checksum
 echo "Verifying MD5 checksum..."
-EXPECTED_MD5=$(cat "$MD5_NAME" | awk '{print $1}')
-ACTUAL_MD5=$(md5sum "$FILE_NAME" | awk '{print $1}')
-
-if [ "$EXPECTED_MD5" == "$ACTUAL_MD5" ]; then
+if md5sum --check "$MD5_NAME"; then
   echo "MD5 checksum verified successfully."
 else
   echo "MD5 checksum verification failed!"
-  echo "Expected: $EXPECTED_MD5"
-  echo "Actual:   $ACTUAL_MD5"
   exit 1
 fi
 
-mv "planet-${PLANET_DATE}.osm.pbf" "$DESINATION_PATH"
+mv "planet-${PLANET_DATE}.osm.pbf" "$DESTINATION_PATH"
