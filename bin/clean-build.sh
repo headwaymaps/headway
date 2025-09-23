c6b676a29bfc8e4e357cd5743098b557e28aa15b
Michael Kirk <michael.code@endoftheworl.de>
2025-09-23 16:14:48 -0700

    simplify clean script

--- bin/clean-build.sh
+++ bin/clean-build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -x
 
-rm -fr data/download
-rm -fr data/generated
-rm -fr data/planet-download
-rm -fr data/maps-earth-planet-v1.*{.osm.pbf,.elasticsearch.tar.zst,.mbtiles,.pelias.json,.placeholder.tar.zst,.valhalla.tar.zst,.graph.obj.zst,.gtfs.tar.zst}
+rm -fr data/
 dagger core engine local-cache prune
 
