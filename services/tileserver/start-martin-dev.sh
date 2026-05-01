677e5985ae53baad6dac52e8834b09c5bdf53ffd
Michael Kirk <michael.code@endoftheworl.de>
2026-05-01 12:17:43 -0700

    fixup martin dev script

--- services/tileserver/start-martin-dev.sh
+++ services/tileserver/start-martin-dev.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-MARTIN_CONFIG=./martin-dev-config.yaml ./configure-and-run.sh
+set -xe
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+MARTIN_CONFIG="$SCRIPT_DIR/martin-dev-config.yaml"
+
+PORT="${PORT:-8000}"
+cat "$MARTIN_CONFIG"
+
+MARTIN_SRC="$SCRIPT_DIR/../../../martin"
+(cd $MARTIN_SRC && cargo build)
+(cd $SCRIPT_DIR && $MARTIN_SRC/target/debug/martin --config "$MARTIN_CONFIG" --listen-addresses "0.0.0.0:${PORT}")
